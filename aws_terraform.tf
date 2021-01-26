6915ce6b5cf68d233583692b084208cdc1c377ed
AnatoliiTymoshchuk <42546387+AnatoliiTymoshchuk@users.noreply.github.com>
2021-01-26 19:27:01 +0200

    Update aws_terraform.tf

--- aws_terraform.tf
+++ aws_terraform.tf
@@ -1,9 +1,9 @@
 provider "m3" {}     
 resource "m3_instance" "my-server" {
-	image = "Debian10_64-bit"   
+	image = "Ubuntu18.04_64-bit"   
 	instance_name = "test_mlti-stack-at"
-	region_name = "AWS-EUWEST"
-	tenant_name = "SOFTLINE"
+	region_name = "AZURE-EUS"
+	tenant_name = "AZURE-MPN"
 	shape = "MINI"
 	key_name = "test_at_smoke1"
 	owner = "mytestjar@gmail.com"      
